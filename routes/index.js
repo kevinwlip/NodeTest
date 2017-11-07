e952832838c3162090f87c9cffcd22ca64290e03
kevinwlip <kevinwlip@gmail.com>
2017-11-06 21:48:52 -0800

    11/6/2017 4

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,12 @@
 var express = require('express');
 var router = express.Router();
 
-var bodyParser = require('body-parser');
-var path = require ('path');
-var querystring = require('querystring');
-
-router.use(bodyParser.urlencoded({ extended: true }));
-
 //now processing post
 router.post('/readNameAndRespond', function(req, res, next) {
 //expecting data variable called name --retrieve value using body-parser
     var value_name = req.body.name;  //retrieve the data associated with name
     res.send("hello " + value_name);
-});
+}
+);
+
+module.exports = router;
